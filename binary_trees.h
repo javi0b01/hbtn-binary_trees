553a8a7d1078a47ea389809325099fb2bfc8e0a9
ja-programmer <javierandres@tecnoayuda.co>
2020-03-09 16:08:47 -0500

    task0betty

--- binary_trees.h
+++ binary_trees.h
@@ -1,4 +1,4 @@
-#ifndef _BINARY_TRESS_H_
+#ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,10 +13,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 /* Basic Binary Tree */
 typedef struct binary_tree_s binary_tree_t;
